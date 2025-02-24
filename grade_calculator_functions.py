71240ce736e2e0b1ced261ef9de51b6223dbe435
IanEstubio <kazuhajean27@gmail.com>
2025-02-24 21:57:38 +0800

    Submitting Python Week 05 - Laboratory # 19

--- grade_calculator_functions.py
+++ grade_calculator_functions.py
@@ -2,12 +2,10 @@
 # Laboratory # 19 - Group Activity # 01 - Problem 01: Enhanced Grade Calculator with Function Decomposition
 
 def get_student_score():
-    score = float(input("Enter your score:"))
+    score = float(input("Enter your score: "))
     return score
-   
 
 def calculate_grade(score):
-    
     if score >= 90:
         return "A"
     elif score >= 80:
@@ -18,8 +16,8 @@ def calculate_grade(score):
         return "D"
     else:
         return "F"
-   
-  
+
+# Main program execution
 score = get_student_score()
 grade = calculate_grade(score)
 print(f"Your Grade is: {grade}")
